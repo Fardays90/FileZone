8d6542dda59dd57446595dc7e7bd84acaa3a6f0f
fardays <fardin1055@gmail.com>
2026-04-14 02:57:48 +0600

    build issue

--- client/FileZone/src/main.tsx
+++ client/FileZone/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 
 if('serviceWorker' in navigator){
     navigator.serviceWorker.register('/serviceWorker.js');
-    await navigator.serviceWorker.ready;
+    // await navigator.serviceWorker.ready;
 }
 createRoot(document.getElementById('root')!).render(
     <App />
